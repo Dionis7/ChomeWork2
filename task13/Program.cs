c8543ecdf418bc968e7867cce30cbeaca7a6acf1
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-10-10 13:54:10 +0300

    create task15

---
+++ task13/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+
+Console.WriteLine(numberS.Length > 2 ? $"{number} -> {numberS[2]}" : $"{numberS} -> третьей цифры нет");
---
+++ task15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели:");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+string isWeekend = (dayOfWeek > 0 && dayOfWeek < 8)
+    ? (dayOfWeek == 6 || dayOfWeek == 7) ? "да" : "нет"
+    : "не день недели";
+
+Console.WriteLine($"{dayOfWeek} -> {isWeekend}");
